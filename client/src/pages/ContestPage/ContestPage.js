cf82154b7ca0f7d99ce4f8d3cba70b88826e1bf9
Wysogota <kmmaxim69@gmail.com>
2022-02-20 17:16:34 +0200

    Make func component

--- client/src/pages/ContestPage/ContestPage.js
+++ client/src/pages/ContestPage/ContestPage.js
@@ -1,42 +1,35 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
 import isEqual from 'lodash/isEqual';
 import LightBox from 'react-image-lightbox';
-import {
-  getContestById,
-  goToExpandedDialog,
-  changeEditContest,
-  changeShowImage,
-} from '../../actions/actionCreator';
+import * as actionCreator from '../../actions/actionCreator';
 import Header from '../../components/Header/Header';
 import ContestSideBar from '../../components/ContestSideBar/ContestSideBar';
-import styles from './ContestPage.module.sass';
-import CONSTANTS from '../../constants';
 import Brief from '../../components/Brief/Brief';
 import Spinner from '../../components/Spinner/Spinner';
 import TryAgain from '../../components/TryAgain/TryAgain';
-import 'react-image-lightbox/style.css';
 import ContestPageViewMode from './ContestPageViewMode';
 import ContestPageOffer from './ContestPageOffer';
+import CONSTANTS from '../../constants';
+import 'react-image-lightbox/style.css';
+import styles from './ContestPage.module.sass';
 
-class ContestPage extends React.Component {
-  componentWillUnmount() {
-    this.props.changeEditContest(false);
-  }
+const ContestPage = (props) => {
+  const { data : {id: userId, role} } = useSelector(({ userStore }) => userStore);
+  const { contestData, isShowOnFull, imagePath, error, isFetching, isBrief, offers } = useSelector(({ contestByIdStore }) => contestByIdStore);
+  const { messagesPreview } = useSelector(({ chatStore }) => chatStore);
 
-  componentDidMount() {
-    this.getData();
-  }
+  const { getContestById, goToExpandedDialog, changeEditContest, changeShowImage } = bindActionCreators(actionCreator, useDispatch());
 
-  getData = () => {
-    const { params } = this.props.match;
-    this.props.getData({ contestId: params.id });
-  };
+  useEffect(() => {
+    const { params } = props.match;
+    getContestById({ contestId: params.id });
+    return () => changeEditContest(false);
+  }, []);
 
-  findConversationInfo = (interlocutorId) => {
-    const { messagesPreview } = this.props.chatStore;
-    const { id } = this.props.userStore.data;
-    const participants = [id, interlocutorId];
+  const findConversationInfo = (interlocutorId) => {
+    const participants = [userId, interlocutorId];
     participants.sort((participant1, participant2) => participant1 - participant2);
     for (let i = 0; i < messagesPreview.length; i++) {
       if (isEqual(participants, messagesPreview[i].participants)) {
@@ -51,68 +44,39 @@ class ContestPage extends React.Component {
     return null;
   };
 
-  goChat = () => {
-    const { User } = this.props.contestByIdStore.contestData;
-    this.props.goToExpandedDialog({
+  const goChat = () => {
+    const { User } = contestData;
+    goToExpandedDialog({
       interlocutor: User,
-      conversationData: this.findConversationInfo(User.id),
+      conversationData: findConversationInfo(User.id),
     });
   };
 
-  render() {
-    const { role } = this.props.userStore.data;
-    const {
-      contestByIdStore,
-      changeShowImage,
-      getData,
-    } = this.props;
-    const {
-      isShowOnFull,
-      imagePath,
-      error,
-      isFetching,
-      isBrief,
-      contestData,
-      offers,
-    } = contestByIdStore;
-    return (
-      <div>
-        {isShowOnFull && (
-          <LightBox
-            mainSrc={`${CONSTANTS.publicURL}${imagePath}`}
-            onCloseRequest={() => changeShowImage({ isShowOnFull: false, imagePath: null })}
-          />
-        )}
-        <Header />
-        {error && <div className={styles.tryContainer}><TryAgain getData={getData} /></div>}
-        {isFetching && <div className={styles.containerSpinner}><Spinner /></div>}
-        {contestData && <div className={styles.mainInfoContainer}>
-          <div className={styles.infoContainer}>
-            <ContestPageViewMode />
-            {isBrief
-              ? <Brief contestData={contestData} role={role} goChat={this.goChat} />
-              : <ContestPageOffer />}
-          </div>
-          <ContestSideBar
-            contestData={contestData}
-            totalEntries={offers.length}
-          />
-        </div>}
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  const { contestByIdStore, userStore, chatStore } = state;
-  return { contestByIdStore, userStore, chatStore };
+  return (
+    <div>
+      {isShowOnFull && (
+        <LightBox
+          mainSrc={`${CONSTANTS.publicURL}${imagePath}`}
+          onCloseRequest={() => changeShowImage({ isShowOnFull: false, imagePath: null })}
+        />
+      )}
+      <Header />
+      {error && <div className={styles.tryContainer}><TryAgain getData={getContestById} /></div>}
+      {isFetching && <div className={styles.containerSpinner}><Spinner /></div>}
+      {contestData && <div className={styles.mainInfoContainer}>
+        <div className={styles.infoContainer}>
+          <ContestPageViewMode />
+          {isBrief
+            ? <Brief contestData={contestData} role={role} goChat={goChat} />
+            : <ContestPageOffer />}
+        </div>
+        <ContestSideBar
+          contestData={contestData}
+          totalEntries={offers.length}
+        />
+      </div>}
+    </div>
+  );
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  getData: (data) => dispatch(getContestById(data)),
-  goToExpandedDialog: (data) => dispatch(goToExpandedDialog(data)),
-  changeEditContest: (data) => dispatch(changeEditContest(data)),
-  changeShowImage: (data) => dispatch(changeShowImage(data)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContestPage);
+export default ContestPage;
