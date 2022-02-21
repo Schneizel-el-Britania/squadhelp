ee75e3770e24ec12c712eb8c8b9ddaaf7c5e3caf
Wysogota <kmmaxim69@gmail.com>
2022-02-21 21:56:59 +0200

    Refactor FormInput component

--- client/src/components/FormInput/FormInput.js
+++ client/src/components/FormInput/FormInput.js
@@ -2,28 +2,25 @@ import React from 'react';
 import { Field, ErrorMessage } from 'formik';
 import classNames from 'classnames';
 
-const FormInput = ({
-  classes, label, name, ...rest
-}) => (
-  <Field name={name}>
-    {(props) => {
-      const {
-        field,
-        meta: { touched, error },
-      } = props;
+const FormInput = (props) => {
+  const { classes, label, name, ...rest } = props;
+  return (
+    <Field name={name}>{
+      (props) => {
+        const { field, meta: { touched, error } } = props;
+        const inputClassName = classNames(classes.input, {
+          [classes.notValid]: touched && error,
+          [classes.valid]: touched && !error,
+        });
 
-      const inputClassName = classNames(classes.input, {
-        [classes.notValid]: touched && error,
-        [classes.valid]: touched && !error,
-      });
-      return (
-        <div className={classes.container}>
-          <input type="text" {...field} placeholder={label} className={inputClassName} {...rest} />
-          <ErrorMessage name={name} component="span" className={classes.warning} />
-        </div>
-      );
-    }}
-  </Field>
-);
-
+        return (
+          <div className={classes.container}>
+            <input type="text" {...field} placeholder={label} className={inputClassName} {...rest} />
+            <ErrorMessage name={name} component="span" className={classes.warning} />
+          </div>
+        );
+      }
+    }</Field>
+  );
+};
 export default FormInput;
