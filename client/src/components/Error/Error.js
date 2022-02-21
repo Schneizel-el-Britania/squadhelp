e6a09077d099c4bbbcac1677e15a9456903fd636
Wysogota <kmmaxim69@gmail.com>
2022-02-21 17:37:52 +0200

    Error refactor

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -2,29 +2,26 @@ import React from 'react';
 import styles from './Error.module.sass';
 
 const Error = (props) => {
+  const { status, data, clearError } = props;
   const getMessage = () => {
-    const { status, data } = props;
     switch (status) {
       case 404:
+      case 406:
+      case 409:
         return data;
       case 400:
         return 'Check the input data';
-      case 409:
-        return data;
       case 403:
         return 'Bank decline transaction';
-      case 406:
-        return data;
       default:
         return 'Server Error';
     }
   };
 
-  const { clearError } = props;
   return (
     <div className={styles.errorContainer}>
       <span>{getMessage()}</span>
-      <i className="far fa-times-circle" onClick={() => clearError()} />
+      <i className="far fa-times-circle" onClick={clearError} />
     </div>
   );
 };
