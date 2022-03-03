a498dfb6f9c3d74d72e5a9547ef434f2e6e1fd52
Wysogota <kmmaxim69@gmail.com>
2022-03-03 14:08:54 +0200

    Refactor Nortification component

--- client/src/components/Notification/Notification.js
+++ client/src/components/Notification/Notification.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import styles from './Notification.module.sass';
 
-const Notification = (props) => (
-  <div>
-    <br />
-    <span>{props.message}</span>
-    <br />
-    {props.contestId
-            && <span onClick={() => props.history.push(`/contest/${props.contestId}`)} className={styles.goToContest}>Go to contest</span>}
-  </div>
-);
+const Notification = (props) => {
+  const { message, contestId, history } = props;
+  const onClickHandler = () => history.push(`/contest/${contestId}`);
+  return (
+    <div>
+      <br />
+      <span>{message}</span>
+      <br />
+      {contestId &&
+        <span onClick={onClickHandler} className={styles.goToContest}>
+          Go to contest
+        </span>}
+    </div>
+  );
+};
 
 export default withRouter(Notification);
